f6aaafcc2989ab5c0becf94794bb01add76102e3
Christopher David <chris@arcade.city>
2023-03-11 10:33:53 -0600

    public key logged

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use nostr_sdk::prelude::*;
+use std::str::FromStr;
 
 const PRIVATE_KEY: &str = "4540484eedb0bc5ba1209cea76ff7b6a77fee473708b113eac726058580267ad";
 
@@ -11,9 +12,18 @@ fn greet(name: &str) -> String {
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    let secret_key = SecretKey::from_str(PRIVATE_KEY)?;
+    let my_keys = Keys::new(secret_key);
+
+    println!(
+        "Hello, nostr!!!!! My public key is: {}",
+        my_keys.public_key().to_string()
+    );
+
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+
     Ok(())
 }
