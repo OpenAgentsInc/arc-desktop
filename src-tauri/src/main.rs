f2caff7aaf80fcd2a4c31363c5f98dad361dc8ed
Christopher David <chris@arcade.city>
2023-03-11 17:57:20 -0600

    axe publishing demo msg

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -25,21 +25,11 @@ async fn main() -> Result<()> {
     let secret_key = SecretKey::from_str(PRIVATE_KEY)?;
     let my_keys = Keys::new(secret_key);
 
-    let message = format!(
-        "Hello from NDK tutorial! My public key is: {}",
-        my_keys.public_key()
-    );
-    println!("{}", message);
-
     let opts = Options::new().wait_for_send(true);
     let client = Client::new_with_opts(&my_keys, opts);
     client.add_relay("wss://relay.damus.io", None).await?;
     client.add_relay("wss://arc1.arcadelabs.co", None).await?;
-
     client.connect().await;
-
-    let event_id = client.publish_text_note(message, &[]).await?;
-    println!("{}", event_id);
 
     // Retrieve a maximum of 25 kind-0 events from the relays
     let filter = Filter {
