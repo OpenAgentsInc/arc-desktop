774c1fe7a6620c7a2cc609efc94d7f65c7ba7cfb
Christopher David <chris@arcade.city>
2023-03-11 10:26:55 -0600

    Asyncify

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,19 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+use nostr_sdk::prelude::*;
+
+const PRIVATE_KEY: &str = "4540484eedb0bc5ba1209cea76ff7b6a77fee473708b113eac726058580267ad";
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<()> {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+    Ok(())
 }
